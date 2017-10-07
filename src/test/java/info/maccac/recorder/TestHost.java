0c66753b8d9f9658d05e1ccf491242660c010d84
Marcel Caroly <mcaroly@gmail.com>
2017-10-07 18:21:37 +1100

    Adding a TestHost to assist in manual testing

---
+++ src/test/java/info/maccac/recorder/TestHost.java
@@ -0,0 +1,40 @@
+package info.maccac.recorder;
+
+import com.jcraft.jsch.Buffer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.stream.Collectors;
+
+public class TestHost {
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/results", new MyHandler());
+        server.start();
+    }
+
+    private static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            String body;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(t.getRequestBody()))) {
+                body = reader.lines().collect(Collectors.joining("\n"));
+            }
+            System.out.println(body);
+
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
